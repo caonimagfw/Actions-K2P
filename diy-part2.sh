71abf142b67da3ebd978feeae9cd5686d8c77469
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2022-11-17 12:54:38 +0800

    init openwrt

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,37 +17,18 @@ set -e
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify default settings
-cp $GITHUB_WORKSPACE/default-settings .
-echo >> default-settings
-sed -n -i -e '/rm -rf \/tmp\/luci-modulecache\//r default-settings' -e 1x -e '2,${x;p}' -e '${x;p}' package/lean/default-settings/files/zzz-default-settings
-rm default-settings
+
 
 # Patch
-for i in $GITHUB_WORKSPACE/patches/*.patch; do patch --fuzz 3 -p0 < $i; done
+# for i in $GITHUB_WORKSPACE/patches/*.patch; do patch --fuzz 3 -p0 < $i; done
 
 # Set permissions
 chmod +x package/feeds/helloworld/luci-app-ssr-plus/root/etc/ssrplus/iptables_config.sh
 
-#打开bbr加速
-sed -i "s/option bbr_cca.*/option bbr_cca '1'/" feeds/luci/applications/luci-app-turboacc/root/etc/config/turboacc
+#修改保留内存到1M
+sed -i '/vm.min_free_kbytes=/d' /etc/sysctl.conf
+echo 'vm.min_free_kbytes=1024' >> /etc/sysctl.conf
 
-#清空ssrplus黑名单
-echo > package/feeds/helloworld/luci-app-ssr-plus/root/etc/ssrplus/deny.list
-
-#ssrplus访问国外域名DNS服务器设为1.0.0.1
-sed -i "s/tunnel_forward=.*/tunnel_forward='1.0.0.1:53'/" feeds/helloworld/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
-
-#ssrplus使用本机5335端口
-sed -i "s/pdnsd_enable=.*/pdnsd_enable='0'/" feeds/helloworld/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
-
-#ssrplus全端口代理
-sed -i "s/dports=.*/dports='1'/" feeds/helloworld/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
-
-#ssrplus关闭自动切换
-sed -i "s/enable_switch=.*/enable_switch='0'/" feeds/helloworld/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
-
-#修改dnsforwarder配置
-cp $GITHUB_WORKSPACE/replace_files/dnsforwarder.config feeds/packages/net/dnsforwarder/files/etc/config/dnsforwarder
-
-#清空dnsforwarder无用文件
-echo > feeds/packages/net/dnsforwarder/files/etc/dnsforwarder/gfw.txt
+# bbr & cake
+echo 'net.ipv4.tcp_congestion_control=bbr' >> package/base-files/files/etc/sysctl.d/10-default.conf
+echo 'net.core.default_qdisc=cake' >> package/base-files/files/etc/sysctl.d/10-default.conf
